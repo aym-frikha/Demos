2bb02db6c6a6b5589bfc45d434cc902700300a4a
Ubuntu <ubuntu@fce.maas>
2019-08-21 15:59:31 +0000

    keystone upgrade correction

--- clouds/maas/upgrade-rocky-stein/step-2-keystone-upgrade.sh
+++ clouds/maas/upgrade-rocky-stein/step-2-keystone-upgrade.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Need to cleanup token using 'juju ssh keystone/leader sudo keystone-manage token_flush' "
 
 
-juju run-action keystone/0 --wait pause
-juju run-action keystone/0 --wait openstack-upgrade
-juju run-action keystone/0 --wait resume
+juju run --unit  keystone/1 "sudo keystone-manage token_flush"
 juju run-action keystone/1 --wait pause
 juju run-action keystone/1 --wait openstack-upgrade
 juju run-action keystone/1 --wait resume
+juju run-action keystone/0 --wait pause
+juju run-action keystone/0 --wait openstack-upgrade
+juju run-action keystone/0 --wait resume
 juju run-action keystone/2 --wait pause
 juju run-action keystone/2 --wait openstack-upgrade
 juju run-action keystone/2 --wait resume
