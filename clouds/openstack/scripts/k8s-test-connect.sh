ca2277584456b3c45417acc615dfcb2390cedc34
Ubuntu <ubuntu@fce.maas>
2019-08-22 14:54:43 +0000

    change test connect script

--- clouds/openstack/scripts/k8s-test-connect.sh
+++ clouds/openstack/scripts/k8s-test-connect.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-while true; do curl   -w "@$PWD/../template/curl-format.txt"  -s -kL http://echoapp.maas/apple ; sleep 1; done
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+while true; do curl   -w "@$DIR/../template/curl-format.txt"  -s -kL http://echoapp.maas/apple ; sleep 1; done
